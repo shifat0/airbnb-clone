ed971abdb252fd8a54e0f1d59f6f32eefe6b3a9d
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-06 16:58:35 +0600

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { HeaderItems } from "./HeaderItems";
+import { faChevronRight, faSliders } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
-    <div className="border-t-2">
-      <div className="container mx-auto fixed">Header</div>
+    <div className="border-t-2 p-5">
+      <div className="container mx-auto flex justify-between items-center">
+        {HeaderItems.map((item, idx) => (
+          <div
+            key={idx}
+            className="flex flex-col items-center cursor-pointer p-2 text-slate-600 border-b-2 border-transparent focus:border-black focus:text-black"
+            tabIndex={idx}
+          >
+            <img src={item.src} alt={item.title} className="w-5 h-5" />
+            <span className="text-xs">{item.title}</span>
+          </div>
+        ))}
+
+        <FontAwesomeIcon
+          icon={faChevronRight}
+          className="w-3 h-3 border-2 p-2 rounded-full cursor-pointer"
+        />
+
+        <button className="flex items-center gap-5 border-2 rounded-xl p-3">
+          <FontAwesomeIcon icon={faSliders} />
+          <span>Filters</span>
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header/HeaderItems.jsx
@@ -0,0 +1,15 @@
+import bed from "../../Assets/bed.jpg";
+import beach from "../../Assets/beach.jpg";
+import luxe from "../../Assets/Luxe.jpg";
+import caves from "../../Assets/caves.jpg";
+import countryside from "../../Assets/countryside.jpg";
+import rooms from "../../Assets/rooms.jpg";
+
+export const HeaderItems = [
+  { src: bed, title: "Bed & breakfast" },
+  { src: rooms, title: "Rooms" },
+  { src: countryside, title: "Countryside" },
+  { src: luxe, title: "Luxe" },
+  { src: beach, title: "Beachfront" },
+  { src: caves, title: "Caves" },
+];
